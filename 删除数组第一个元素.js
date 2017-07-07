f8fbeb59afd0b827c539f10771a9bcab460481ef
JinHuiLai <laijinhui1202@gmail.com>
2017-07-07 09:40:10 +0800

    数组

---
+++ 删除数组第一个元素.js
@@ -0,0 +1,37 @@
+//利用slice
+function curtail(arr) {
+    return arr.slice(1);
+}
+//利用filter
+function curtail(arr) {
+    return arr.filter(function(v,i) {
+        return i!==0;
+    });
+}
+//利用push.apply+shift
+function curtail(arr) {
+    var newArr=[];
+    [].push.apply(newArr, arr);
+    newArr.shift();
+    return newArr;
+}
+//利用join+split+shift    注意！！！：数据类型会变成字符型
+function curtail(arr) {
+    var newArr = arr.join().split(',');
+    newArr.shift();
+    return newArr;
+}
+//利用concat+shift
+function curtail(arr) {
+    var newArr = arr.concat();
+    newArr.shift();
+    return newArr;
+}
+//普通的迭代拷贝
+function curtail(arr) {
+    var newArr=[];
+    for(var i=1;i<arr.length;i++){
+        newArr.push(arr[i]);
+    }
+    return newArr;
+}
