f3c20bfb1ae2e1b87a2e2027115f3b06774ecda4
JinHuiLai <laijinhui1202@gmail.com>
2017-07-07 09:53:18 +0800

    数组

---
+++ 查找重复元素.js
@@ -0,0 +1,19 @@
+function duplicates(arr) {
+     //声明两个数组，a数组用来存放结果，b数组用来存放arr中每个元素的个数
+     var a = [],b = [];
+     //遍历arr，如果以arr中元素为下标的的b元素已存在，则该b元素加1，否则设置为1
+     for(var i = 0; i < arr.length; i++){
+         if(!b[arr[i]]){
+             b[arr[i]] = 1;
+             continue;
+         }
+         b[arr[i]]++;
+     }
+     //遍历b数组，将其中元素值大于1的元素下标存入a数组中
+     for(var i = 0; i < b.length; i++){
+         if(b[i] > 1){
+             a.push(i);
+         }
+     }
+     return a;
+ }
