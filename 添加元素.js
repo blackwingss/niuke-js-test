1ac181059ccf2ad694535add951c62a8f6a9df70
JinHuiLai <laijinhui1202@gmail.com>
2017-07-07 09:38:14 +0800

    数组

--- 添加元素.js
+++ 添加元素.js
@@ -1,39 +1,33 @@
-链接： https: //www.nowcoder.com/questionTerminal/3c7905cea3264ddaac4bf637ab3a19f9
-  来源： 牛客网
-
-/**
- * 普通的迭代拷贝
- * @param arr
- * @param item
- * @returns {Array}
- */
-var append = function(arr, item) {    
-  var length = arr.length,
-            newArr = [];     
-  for (var i = 0; i < length; i++) {        
-    newArr.push(arr[i]);    
-  }     
-  newArr.push(item);     
+//利用concat
+function prepend(arr, item) {    
+  return [item].concat(arr);
+}
+//使用push.apply
+function prepend(arr, item) {    
+  var newArr = [item];    
+  [].push.apply(newArr, arr);    
   return newArr;
-}; 
-/**
- * 使用slice浅拷贝+push组合
- * @param arr
- * @param item
- * @returns {Blob|ArrayBuffer|Array.<T>|string}
- */
-var append2 = function(arr, item) {    
-  var newArr = arr.slice(0);  // slice(start, end)浅拷贝数组
+}
+//利用slice+unshift/splice
+function prepend(arr, item) {    
+  var newArr = arr.slice(0);    
+  newArr.unshift(item); //newArr.splice(0,0,item);
       
-  newArr.push(item);    
   return newArr;
-}; 
-/**
- * 使用concat将传入的数组或非数组值与原数组合并,组成一个新的数组并返回
- * @param arr
- * @param item
- * @returns {Array.<T>|string}
- */
-var append3 = function(arr, item) {    
-  return arr.concat(item);
-};
+}
+//使用join+split+unshift/splice组合
+function prepend(arr, item) {    
+  var newArr = arr.join().split(',');    
+  newArr.unshift(item); //newArr.splice(0,0,item);
+      
+  return newArr;
+}
+//普通的迭代拷贝
+function prepend(arr, item) {    
+  var newArr = [];    
+  for (var i = 0; i < arr.length; i++) {        
+    newArr.push(arr[i]);    
+  }    
+  newArr.unshift(item);    
+  return newArr;
+}
